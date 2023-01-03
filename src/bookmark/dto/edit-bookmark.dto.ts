ca400da743382e85a5465d3f8c75b0e8930c5b13
niuxiangchen <1415055998@qq.com>
2023-01-03 21:28:47 +0800

    feat: 代码提交

---
+++ src/bookmark/dto/edit-bookmark.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class EditBookmarkDto {
+  @IsString()
+  @IsOptional()
+  title?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  link?: string;
+}
